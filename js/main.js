9c5762886bab4d4e2769fdf36cd0adbb1a01b7b7
Marta Morán López <martamoranl06@gmail.com>
2022-04-18 19:22:59 +0200

    forms login y registro funcionando

--- js/main.js
+++ js/main.js
@@ -1,40 +1,151 @@
+const minusculas = "abcdfghijklmnñopqrstuvwxyz";
+const mayusculas = minusculas.toLocaleUpperCase();
+const especiales = "!?¿¡/()&¬%$·#+^[]{};.";
+const numeros = "1234567890";
 
+/*
+class Usuario {
+    constructor(nombre, nuevoNombreUsuario, nuevaClaveusuario) {
+        this.usuario = nombre;
+        this.clave = nuevoNombreUsuario;
+        this.nombre = nuevaClaveusuario;
+    }
+}
+*/
 
 const app = Vue.createApp({
     data() {
         return {
-            message: null,
+            result: null,
             usuario: null,
             clave: null,
             error: null,
+            usuarios: new Map(),
+            newName: "",
+            newUserName: "",
+            newPass: "",
+            repeatNewPass: "",
+            nuevoUsuario: [],
+
         };
     },
+    created() {
+        fetch("json/usuarios.json")
+            .then(response => response.json())
+            .then(response => this.usuarios = response)
+    },
     methods: {
-       async loginUsuario() {
-            const response = await fetch("json/usuarios.json")
+        async loginUsuario() {
+            /*const response = await fetch("json/usuarios.json");
             const data = await response.json();
-            //let nombreUsuario;
-            try {
-                for (user of data) {
-                    if (this.usuario == user.usuario && this.clave == user.clave) {
-                        this.message = user.nombre;
-                        //location.href = "principal.html";
-                        console.log(this.message)
-                        //habría que mandar el user.nombre a una variable para q luego se muestre en la página principal
-                    }
-                }
-                
-            } catch (error) {
-                this.error = error;
-            }
+            this.usuarios = data;
+            console.log(this.usuarios);*/
+            let existe = false;
 
+            for (user of this.usuarios) {
+                if (this.usuario == user.usuario && this.clave == user.clave) {
+                    this.result = user.nombre;
+                    existe = true;
+                    console.log(user.nombre)
+                    console.log(this.usuarios)
+                    //location.href = "principal.html";
+                    //habría que mandar el user.nombre a una variable para q luego se muestre en la página principal
+                }
+            }
+            if (!existe) {
+                this.error = "Usuario o contraseña incorrectos";
+            }
         },
         registroUsuario() {
             location.href = "index-registro.html";
+        },
+
+        validarNuevoUsuario() {
+            let especialesOk = true;
+            let mayusculasOk = true;
+            let userNameRepetido = false;
+
+            for (user of this.usuarios) {
+                if (this.newUserName == user.usuario) {
+                    userNameRepetido = true
+                    this.error = "El nombre de usuario ya existe"
+                }
+            }
+
+
+            for (let i = 0; i < especiales.length; i++) {
+                for (let j = 0; j < mayusculas.length; j++) {
+                    if (this.newUserName.includes(especiales.charAt(i))) {
+                        especialesOk = false;
+                    }
+                    if (this.newUserName.includes(mayusculas.charAt(j))) {
+                        mayusculasOk = false;
+                    }
+                }
+            }
+
+            if(!mayusculasOk || !especialesOk) {
+                this.error = "El nombre de usuario no puede contener mayúsculas y/o carcateres especiales distintos de _ o -"
+            }
+
+            if (especialesOk && mayusculasOk && !userNameRepetido) {
+                return true;
+            }
+        },
+
+        validarClaveNuevoUsuario() {
+            let especialesOk = false;
+            let mayusculasOk = false;
+            let numeroOk = false;
+
+
+            if (this.newPass != this.repeatNewPass) {
+                this.error = "Las contraseñas no coinciden";
+            }
+
+            for (let i = 0; i < mayusculas.length; i++) {
+                for (let j = 0; j < especiales.length; j++) {
+                    for (let k = 0; k < numeros.length; k++) {
+                        if (this.newPass.includes(mayusculas.charAt(i))) {
+                            mayusculasOk = true;
+                        }
+                        if (this.newPass.includes(especiales.charAt(j))) {
+                            especialesOk = true;
+                        }
+                        if (this.newPass.includes(numeros.charAt(k))) {
+                            numeroOk = true;
+                        }
+                    }
+                }
+            }
+
+            if (!especialesOk || !mayusculasOk || !numeroOk) {
+                this.error = "la contraseña debe contener al menos 6 caracteres y al menos 1 mayúscula, 1 caracter especial y 1 número"
+            }
+
+            if (this.newPass.length > 6 && mayusculasOk  && especialesOk  && numeroOk) {
+                return true;
+            }
+        },
+        validarUsuarioyClave() {
+            this.validarNuevoUsuario();
+            this.validarClaveNuevoUsuario();
+
+            if (this.validarNuevoUsuario() && this.validarClaveNuevoUsuario()) {
+                console.log("Usuario creado correctamente");
+                this.nuevoUsuario =  {"usuario":this.newUserName,"clave":this.newPass, "nombre": this.newName} 
+                this.usuarios.push(this.nuevoUsuario)
+                
+            } else if (this.newName == "" || this.newUserName == "" || this.newPass == "" || this.repeatNewPass == "") {
+                this.error = "Por favor, rellena todos los campos"
+            }
+
+          
+           console.log(this.usuarios);
+
         }
     }
-       
-});
+})
 
 /*
 function crearMapa() {
@@ -59,4 +170,4 @@ crearMapa();
 
 */
 
-        /**/
+/**/
